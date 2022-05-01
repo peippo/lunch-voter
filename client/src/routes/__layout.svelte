c4d15a29867412ca521cbd115dcbac20112ae211
Janne Peippo <peippo@gmail.com>
2022-05-01 17:17:09 +0300

    Add city quicklinks nav

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,5 +1,25 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
+
+	const citySuggestions = ['helsinki', 'turku', 'tampere'];
 </script>
+
+<header>
+	<nav aria-label="Quicklinks">
+		<ul class="flex">
+			{#each citySuggestions as city}
+				<li class="m-3">
+					<a
+						sveltekit:prefetch
+						href={`/city/${city}`}
+						class="capitalize border-pink-500"
+						class:border-b-4={$page.params.city === city}>{city}</a
+					>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
 
 <slot />
