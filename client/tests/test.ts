2f2948785e38d9a69da4178169b738858ea4876d
Janne Peippo <peippo@gmail.com>
2022-05-02 21:23:22 +0300

    Add tests

--- client/tests/test.ts
+++ client/tests/test.ts
@@ -1,6 +1,31 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test.describe('Home', () => {
+	test('should display city search input', async ({ page }) => {
+		await page.goto('/');
+		await expect(page.locator('#city-search')).toBeVisible();
+	});
+});
+
+test.describe('Search', () => {
+	test('should open city page on valid city name submit', async ({ page }) => {
+		await page.goto('/');
+		await page.fill('#city-search', 'helsinki');
+		await page.locator('form button').click();
+		await expect(page.locator('h1')).toHaveText('helsinki');
+	});
+
+	test('should open city page on quick link click', async ({ page }) => {
+		await page.goto('/');
+		const quickLinkNav = page.locator('header nav');
+		await quickLinkNav.locator('text=Turku').click();
+		await expect(page.locator('h1')).toHaveText('turku');
+	});
+});
+
+test.describe('City page', () => {
+	test('should list restaurants', async ({ page }) => {
+		await page.goto('/city/helsinki');
+		await expect(page.locator('ul:below(h1)')).not.toBeEmpty();
+	});
 });
