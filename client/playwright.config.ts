bae349b0558ade3219907abf32dc793ebcc19b38
Janne Peippo <peippo@gmail.com>
2022-05-21 20:35:38 +0300

    Adjust Playwright setup

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,6 +1,47 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 const config: PlaywrightTestConfig = {
+	testDir: './tests',
+	timeout: 30 * 1000,
+	expect: {
+		timeout: 5000
+	},
+	fullyParallel: true,
+	reporter: 'list',
+	use: {
+		actionTimeout: 0,
+		baseURL: 'http://localhost:3000',
+		trace: 'on-first-retry'
+	},
+	projects: [
+		{
+			name: 'firefox',
+			use: {
+				...devices['Desktop Firefox']
+			}
+		}
+		// {
+		// 	name: 'chromium',
+		// 	use: {
+		// 		...devices['Desktop Chrome']
+		// 	}
+		// }
+		// {
+		// 	name: 'webkit',
+		// 	use: {
+		// 		...devices['Desktop Safari']
+		// 	}
+		// }
+	],
+
+	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
+	outputDir: 'test-results/',
+
+	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 3000
