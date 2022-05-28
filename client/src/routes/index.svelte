911f7987a16e706fbfce54118cf5da2ed5920e74
Janne Peippo <peippo@gmail.com>
2022-05-28 10:56:41 +0300

    Set frontpage to prerender

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script>
 	import { fly, fade } from 'svelte/transition';
 	import FrontpageIllustration from '$lib/components/frontpageIllustration.svelte';
